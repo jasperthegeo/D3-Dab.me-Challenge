2a75b9082dc5a27a170a663319deef9b2a4697cb
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-01-27 11:34:33 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
-// @TODO: YOUR CODE HERE!
+//This is the D3 Dabbler Challenge
+//I will initially write the code for the static graph
+//then modify as needed for the animated pseudo-3D graph
+
+//Import required data
+var file = "assets/data/data.csv"

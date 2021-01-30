6292d41affa09e89a0178173941b1eff4ca89771
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-01-29 21:11:30 -0500

    Set up Canvas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,22 @@
 //The initial app kept throwing unresolvable errors, so I have chosen a second 
 //path to coding
 
+
+//Set-up Canvas
+var svgWidth = 1200; //window.innerWidth;
+var svgHeight = 600; //window.innerHeight;
+
+var margin = {
+    top: 20,
+    bottom: 80,
+    right: 40,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var w = parseInt(d3.select("#scatter").style("width")) + margin.left + margin.right;
+
+
 //Import Data

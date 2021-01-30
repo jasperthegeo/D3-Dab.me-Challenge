b153777f894f95a3c1e9ec10620900dd9656080f
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-01-29 21:38:28 -0500

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,3 +26,22 @@ var svg = d3
     .attr("height", svgHeight)
 
 //Import Data
+// import the data from /data/data.csv
+d3.csv("assets/data/data.csv").then((healthData) => {
+
+    // parse the data to numeric values
+    healthData.forEach(function(data) {
+
+        // healthcare vs poverty
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+        
+        // smokers vs age
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+
+        // obesity vs household income
+        data.obesity = +data.obesity;
+        data.income = +data.income;
+
+    });
